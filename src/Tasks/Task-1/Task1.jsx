26a15d570a9d2db4da92726bec6f3477fd33eccf
Hamdan <hamdankhan212@gmail.com>
2023-04-11 18:40:14 +0500

    Link added

--- src/Tasks/Task-1/Task1.jsx
+++ src/Tasks/Task-1/Task1.jsx
@@ -4,11 +4,15 @@ const Task1 = () => {
   return (
     <div className="flex flex-col items-center h-screen">
       <h1 className="text-2xl font-bold">Routing in React</h1>
-      <h1>
+      <h1 className="text-lg">
         The Routing of this interface is built using React-Router-DOM. Navigate
         through the Website using navbar.
-        <br /> View the code of routing at this link:
-        <a href="" className="underline">
+        <br /> View the code of routing at this link:{" "}
+        <a
+          href="https://github.com/Hamdan-Khan/Fellowship-Interface/blob/master/src/App.jsx"
+          className="underline"
+          target="_blank"
+        >
           Code Link
         </a>
       </h1>
