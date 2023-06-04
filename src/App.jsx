9d2def8e72375ea138e3cd56caa0ffa96f8175b2
Hamdan <hamdankhan212@gmail.com>
2023-06-04 12:04:46 +0500

    Dynamic Routing and automation implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import Task1 from "./Tasks/Task-1/Task1";
 import Home from "./Pages/Home";
-import Task2 from "./Tasks/Task-2/Task2";
-import Tasks from "./Pages/Tasks";
 import Navbar from "./components/Navbar";
-import Task3 from "./Tasks/Task-3/Task3";
-import Task4 from "./Tasks/Task-4/Task4";
-import Task5 from "./Tasks/Task-5/Task5";
-import Task6 from "./Tasks/Task-6/Task6";
-import Task7 from "./Tasks/Task-7/Task7";
-import Task8 from "./Tasks/Task-8/Task8";
-import Task9 from "./Tasks/Task-9/Task9";
-import Task10 from "./Tasks/Task-10/Task10";
-import Task11 from "./Tasks/Task-11/Task11";
-import Task12 from "./Tasks/Task-12/Task12";
+import Tasks from "./Pages/Tasks";
+import React, { Suspense } from "react";
+import { tasks } from "./context/tasks";
 
 function App() {
   return (
@@ -22,20 +12,35 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tasks" element={<Tasks />} />
-        <Route path="/tasks/task-1" element={<Task1 />} />
-        <Route path="/tasks/task-2" element={<Task2 />} />
-        <Route path="/tasks/task-3" element={<Task3 />} />
-        <Route path="/tasks/task-4" element={<Task4 />} />
-        <Route path="/tasks/task-5" element={<Task5 />} />
-        <Route path="/tasks/task-6" element={<Task6 />} />
-        <Route path="/tasks/task-7" element={<Task7 />} />
-        <Route path="/tasks/task-8" element={<Task8 />} />
-        <Route path="/tasks/task-9" element={<Task9 />} />
-        <Route path="/tasks/task-10" element={<Task10 />} />
-        <Route path="/tasks/task-11" element={<Task11 />} />
-        <Route path="/tasks/task-12" element={<Task12 />} />
+        <>
+          {tasks.map((task) => (
+            <Route
+              key={task.link}
+              path={`/tasks/${task.link}`}
+              element={
+                <TaskWrapper taskNum={task.taskNum} taskLink={task.link} />
+              }
+            />
+          ))}
+        </>
       </Routes>
     </div>
+  );
+}
+
+function TaskWrapper({ taskNum, taskLink }) {
+  const TaskComponent = React.lazy(() =>
+    import(`./Tasks/Task-${taskNum}/Task${taskNum}`)
+  );
+
+  return (
+    <Suspense
+      fallback={
+        <div className="text-center font-bold text-3xl mt-5">Loading...</div>
+      }
+    >
+      <TaskComponent />
+    </Suspense>
   );
 }
 
--- src/context/tasks.js
+++ src/context/tasks.js
@@ -1,7 +1,4 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const tasks = [
+export const tasks = [
   { title: "Routing in React", taskNum: 1, link: "task-1" },
   { title: "React Hooks- useContext & useRef", taskNum: 2, link: "task-2" },
   { title: "React Hooks- useMemo & useReducer", taskNum: 3, link: "task-3" },
@@ -20,25 +17,3 @@ const tasks = [
   { title: "Forms Validation", taskNum: 12, link: "task-12" },
   { title: "React Hook Form", taskNum: 13, link: "task-13" },
 ];
-
-const Tasks = () => {
-  return (
-    <div className="flex flex-col items-center min-h-[450px] pb-6 border justify-center shadow-2xl rounded-xl m-8">
-      <h1 className="text-3xl font-semibold my-4">LINKS TO TASKS:</h1>
-      <ul className="text-xl flex flex-col gap-2">
-        {tasks.map((task) => {
-          const { title, taskNum, link } = task;
-          return (
-            <li key={taskNum}>
-              <Link to={`/tasks/${link}`}>
-                &#8658; <span className="underline">{title}</span>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
-
-export default Tasks;
